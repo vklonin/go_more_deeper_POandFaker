9f85475a60a856143d095ef1e1c1975b574417d8
vladimir klonin <vklonin@gmail.com>
2021-08-09 10:16:13 +0300

    final commit

---
+++ src/test/java/components/CalendarPicker.java
@@ -0,0 +1,16 @@
+package components;
+
+import static com.codeborne.selenide.Selenide.$;
+
+
+public class CalendarPicker {
+
+    public void setDate(String day, String month, String year) {
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption(month); // month -1
+        $(".react-datepicker__year-select").selectOptionByValue(year);
+        $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
+    }
+
+}
