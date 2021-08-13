b71c4d9652e835da72d1c85204830edf7fa2557d
vladimir klonin <vklonin@gmail.com>
2021-08-13 11:14:32 +0300

    Updated email with faker domain name
    Updated mobile with faker
    
    Updated email with first and last names

--- src/test/java/utils/TestData.java
+++ src/test/java/utils/TestData.java
@@ -14,8 +14,8 @@ public class TestData {
 
     public String firstName = faker.name().firstName();
     public String lastName = faker.name().lastName();
-    public String cellPhone = "1234567890";
-    public String emailAddress = faker.internet().emailAddress();
+    public String cellPhone = faker.number().digits(10);
+    public String emailAddress = faker.name().firstName() + "." + faker.name().lastName() + faker.internet().domainName();
     public String gender = genderList[random.nextInt(genderList.length-1)];
     public String[] dateOfBirth = ("30 April 2001").split(" ");
     public String[] subjects = {"Maths", "Chemistry"};
